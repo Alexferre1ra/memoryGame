6296393a75baee23c02c83f68788c7ba02265716
DevWeb <alexandreferreira540@gmail.com>
2022-03-26 17:37:55 +0100

    Levels and tile's creation finished; Beginning of random colors

--- memoryGame.js
+++ memoryGame.js
@@ -1,3 +1,7 @@
+/*===========================================
+               Query Selectors
+  ===========================================*/
+
 function $(selector, el) {
   if (!el) {
     el = document;
@@ -11,31 +15,79 @@ function $$(selector, el) {
   return Array.prototype.slice.call(el.querySelectorAll(selector));
 }
 
-// Creating button in relation to level selected
-  let levels = $$(".level");
-  const container = $("div .container");
-  // console.log(levels);
+/*===========================================
+    Creating tiles in relation to level set
+  ===========================================*/
+function newGame() {
+  const levels = $$(".level"); // levels btns
+  const container = $(".container"); // tile's container
+  const heading = document.getElementById("heading"); // heading h1
+
+  // creating new tile
+  function newTile() {
+    newBtn = document.createElement("button");
+    newBtn.className = "tile";
+  }
+
+  // each level set a number of tile
   for (let i = 0; i < levels.length; i++) {
-    levels[i].addEventListener("click", function() {
+    levels[i].addEventListener("click", function () {
       switch (levels[i].value) {
         case "easy":
-          // console.log("button easy");
-          let lines = document.createElement("div");
-          lines.classList.add('line');
-          let buttons = document.createElement("button");
-          buttons.classList.add()
-          container.appendChild(lines*2);
+          // console.log("easy button");
+          for (i = 0; i < 4; i++) {
+            newTile();
+            container.appendChild(newBtn);
+          }
           break;
-          case "intermediate":
-            // console.log("button intermediate")
+
+        case "intermediate":
+          // console.log("intermediate button");
+          for (i = 0; i < 6; i++) {
+            newTile();
+            container.style.width = "324px";
+            container.appendChild(newBtn);
+          }
           break;
-          case "hard":
-            // console.log("button hard")
+
+        case "hard":
+          // console.log("hard button");
+          for (i = 0; i < 9; i++) {
+            newTile();
+            container.style.width = "324px";
+            container.appendChild(newBtn);
+          }
           break;
-      
+
         default:
           break;
       }
-    });;
-    
+      // disabled the levels choise
+      levels.forEach((element) => {
+        element.setAttribute("disabled", true);
+      });
+
+      //change the heading content when game start
+      heading.innerHTML = "Now Repeat the sequencies";
+      chColors();
+    });
   }
+}
+
+/*===========================================
+      Change the color of tiles randomly
+  ===========================================*/
+function chColors() {
+  //  collect buttons
+  const buttons = $$(".tile");
+  //  console.log(buttons);
+
+  let code = new Array();
+  for (let i = 0; i < buttons.length; i++) {
+    let random = Math.floor(Math.random() * buttons.length);
+    code[i] = buttons[random];
+  }
+  return code;
+}
+
+newGame();
