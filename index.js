f7e554029f09b6ed08538e6a6a7a4cb132d5a19a
DevWeb <alexandreferreira540@gmail.com>
2022-04-04 12:35:05 +0200

    It works as hell

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
+// init new game
 newGame();
 
-// Clear
-arrTiles = []; // reset sequence
-level.textContent = arrTiles.length;
-check_sequence(); // reset when no parameters
 
-arrTiles.push(get_random_tile(tiles)); // add the first one
-
+arrTiles.push(get_random_tile()); // add the first one
 
 // Last best winner
 if (localStorage.getItem("name")) {
   record.textContent = `${localStorage.getItem("name")} reached 
-  	level ${localStorage.getItem("level")}`;
+level ${localStorage.getItem("level")}`;
 }
 
-// Reload everything:
-function reload() {
-  reload = location.reload();
-}
 // Event listeners for reload
 reloadButton.addEventListener("click", reload, false);
